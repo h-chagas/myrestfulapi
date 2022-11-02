5b0cd3550391565c42a25f869138769749c184c7
Hugo Chagas <hugochagasuk@gmail.com>
2022-11-02 15:00:12 +0000

    create index.js file

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+
+app.use( express.json() );
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: "Yellow t-shirt",
+        size: "L",
+    })
+})
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `This tshirt with your ${logo} and ID of ${id}`,
+
+    })
+})
+
+
+module.exports = app;
