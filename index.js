9a97687bd7a97cf08763cb31bce880ab831396e1
Hugo Chagas <hugochagasuk@gmail.com>
2022-11-02 17:04:27 +0000

    delete unwanted code + add error handling for route not found

--- index.js
+++ index.js
@@ -7,25 +7,8 @@ app.use( express.json() );
 //GET requests
 app.use('/users', apiRouter);
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: "Yellow t-shirt",
-        size: "L",
-    })
-})
-
-app.post('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
-
-    if (!logo) {
-        res.status(418).send({ message: 'We need a logo!' })
-    }
-
-    res.send({
-        tshirt: `This tshirt with your ${logo} and ID of ${id}`,
-
-    })
+app.all('/*', (req, res) => {
+    res.status(404).send({msg: 'route not found'})
 })
 
 
