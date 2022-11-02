f9f420bcf880b62d14e51c7bdc76a937c348abf9
Hugo Chagas <hugochagasuk@gmail.com>
2022-11-02 16:44:00 +0000

    add DELETE method

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,43 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-// app.use( express.json() );
+let users = [
+   {
+      firstName: "Robbie",
+      lastname: "Williams",
+      city: "London",
+   },
+];
 
-const users = [
-    {
-        firstName: 'Robbie',
-        lastname: 'Williams',
-        city: 'London',
-    },
-]
+apiRouter.get("/", (req, res) => {
+   res.send(users);
+});
 
-apiRouter.get('/', (req, res) => {
-    res.send(users)
-})
+apiRouter.get("/:id", (req, res) => {
+   const { id } = req.params;
+   const findUser = users.filter((user) => {
+      return user.id == id;
+   });
+   res.send(findUser);
+});
 
-apiRouter.post('/', (req, res) => {
-    const user = req.body;
+apiRouter.post("/", (req, res) => {
+   const user = req.body;
 
-    const Id = uuidv4();
-    const UserId={...user, id: Id};
+   const Id = uuidv4();
+   const UserId = { ...user, id: Id };
 
-    users.push(UserId);
-    res.send(`User ${user.firstName} ${user.lastname} has been added.`)
+   users.push(UserId);
+   res.send(`User ${user.firstName} ${user.lastname} has been added.`);
+});
 
-
-})
-
+apiRouter.delete("/:id", (req, res) => {
+   const { id } = req.params;
+   users = users.filter((user) => {
+      return user.id != id;
+   });
+   res.send(`The user with id ${id} has been deleted.`);
+});
 
 module.exports = apiRouter;
